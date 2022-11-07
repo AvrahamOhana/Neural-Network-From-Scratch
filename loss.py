7ec23a5e8ed2697cfbac4630bb418645483475e1
AvrahamOhana <amiko617@gmail.com>
2022-11-07 21:29:32 +0200

    add notes

--- loss.py
+++ loss.py
@@ -12,7 +12,7 @@ class Loss():
 
         # Return the calculated data loss
         return loss_data
-    
+ 
 class CategoricalCrossentropy(Loss):
     # Forward pass
     def forward(self, y_pred, y_true):
